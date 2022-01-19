203fc7cdf8ed57c713789a62e429b3329adeb563
macoyshev <make.oynoshev@gmail.com>
2022-01-19 09:39:29 +0300

    frontend for online chat

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 3000
+
+http.createServer((req, res) => {
+    console.log("Server started on port " + PORT);
+    fs.readFile("index.html", function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}).listen(PORT);
